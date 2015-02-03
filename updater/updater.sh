26d53045eaa38f7d4a78ed73e6cf46759caa46df
Michael Laraia <micheal.laraia@gmail.com>
2015-02-02 20:31:19 -0600

    automatically updates the sql database

---
+++ updater/updater.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd $HOME/extemp/repo/updater
+echo "dumping server database"
+name=forensics-$(date +"%Y-%m-%d").sql
+ssh extemp-update & ssh_pid=$!
+sleep 100
+mysqldump -u extemp --password="EwgEjDX289qyBs3" -h localhost -P 3310 --opt forensics > ${name}
+kill -9 $ssh_pid
+echo "importing new database"
+mysql -u extemp --password="zhH/5]qAYTNNXgE" forensics < ${name}
+echo "indexing"
+sudo indexer --all
