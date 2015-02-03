ad5322db51ee6b8583ac6f850c2bcf9650d789f7
Michael Laraia <micheal.laraia@gmail.com>
2015-02-02 20:45:37 -0600

    mysql protocol tcp

--- updater/updater.sh
+++ updater/updater.sh
@@ -4,7 +4,7 @@ echo "dumping server database"
 name=forensics-$(date +"%Y-%m-%d").sql
 ssh extemp-update & ssh_pid=$!
 sleep 100
-mysqldump -u extemp --password="EwgEjDX289qyBs3" -h localhost -P 3310 --opt forensics > ${name}
+mysqldump -u extemp --password="EwgEjDX289qyBs3" -h localhost -P 3310 --protocol=tcp --opt forensics > ${name}
 kill -9 $ssh_pid
 echo "importing new database"
 mysql -u extemp --password="zhH/5]qAYTNNXgE" forensics < ${name}
