e4e21edb22ce8ef73f5869e5972d4dfdd6690331
Michael Laraia <micheal.laraia@gmail.com>
2015-03-05 09:53:47 -0600

    commented for easier reading

--- crawler/cheetah.py
+++ crawler/cheetah.py
@@ -3,18 +3,22 @@ import pymysql, logging
 logger = logging.getLogger(__name__)
 
 class Cheetah:
-#  log the init of cheetah
    def __init__(self):
+#     log cheetah initialization
       logger.info('Cheetah initializing')
 
-   """gets article information from table. get is mysql select string
-#  expr is mysql command following FROM DB. if 'tags' is in get,
-#  then the tags for the specified article is put into the
-#  returned dictionary"""
+   """gets article information from table.
+#  get is what's to be selected with mysql select 'get'
+#  expr is mysql command following after SELECT 'get' FROM DB.
+#  if 'tags' is in get, then the tags for the specified article is put into the returned dictionary"""
    def getData(self, get='*', expr='where 1', override=None, dic=True, **kwargs):
+#     connects to mysql databa
       self.connect()
+#     determines whether to use dictionary or regular cursor
       if dic: cur = self.cur_dict
       else: cur = self.cur
+#     override useful for debugging the tag system.
+#     override variable is the mysql string to be used
       if override:
          tags = True
          req = override
@@ -25,56 +29,69 @@ class Cheetah:
                get = get.replace(' ','')
                get = get.split(',')
 #        takes tags out of select query and raises flag
+#!       do I have to distinguish between 'tags,' and ',tags'? Would the remaining ',' cause problems
             if 'tags' in get:
                tags = True
                get.remove('tags')
-#        makes sure 'id' is in select query
+#        makes sure 'id' is in select query, but only if tags flag is raised
             if tags and 'id' not in get:
                get.append('id')
             get = ','.join(get)
-#     raises tag if select query is '*' wildcard
+#     raises tags flag if selecting with '*' wildcard
          else: tags = True
-#     create and execute mysql query string
-         req = "Select " + get + " from Articles " + expr
+#     build and execute mysql query string
+         req = "SELECT %s FROM Articles %s" % (get, expr)
+         #req = "Select " + get + " from Articles " + expr
          if 'order' in kwargs:
-            req += " order by " + order
+            req += " order by %s" % order
          if 'limit' in kwargs:
-            req += " limit " + limit
-      print('request: %s' % req)
+            req += " limit %s" % limit
       logger.debug('request: %s' % req)
+
+#     execute mysql query
       try:
          cur.execute(req)
       except:
+#        makes sure that request string gets logged. Actual error doesn't print it
          logger.warning('error with request: %s' % req)
          raise
+#     collects data from the PyMYSQL cursor
       data = cur.fetchall()
+#     makes sure to get the tags if flag was raised
       if tags:
          for item in data:
             item['tags'] = self.getArticleTags(item['id'], False)
+#     closes mysql connection
       self.close()
       return data
 
-#  get tag ids with given article_ids from db article2tags
+#  get id numbers of tags referenced in table article2tags by article with given id
    def getArticleTagIDs(self, article_id, connect=True):
+#     checks if a new connection should be opened
       if connect: self.connect()
       self.cur.execute("SELECT tag FROM article2tags WHERE article=%i" % article_id)
       data = self.cur.fetchall()
       tags = [i[0] for i in data]
       logger.debug('article_id: %i :: tags: %s' % (article_id, tags))
+#     checks if a the connection should be closed
       if connect: self.close()
       return tuple(tags)
 
-#  returns array of tags from given article_id
+#  returns list of tags from given article_id
    def getArticleTags(self, article_id, connect=True):
       if connect: self.connect()
-#     get tag_ids
+#     get tag id numbers
       tag_ids = self.getArticleTagIDs(article_id, False)
+#     if tag_ids is an empty tuple, no tags associated with article
       if tag_ids == tuple(): return None
+#     only one tag associated with article
       if len(tag_ids) == 1:
          q="SELECT tagName from tags where tagID = %s" % tag_ids[0]
+#     multiple tags associated with article
       else:
          q="SELECT tagName from tags where tagID in %s" % (tag_ids,)
       logger.debug('query: %s' % q)
+#     execute query and get data from cursor
       self.cur.execute(q)
       tags = self.cur.fetchall()
 #     convert data from tuple of tuples: ((1,),(2,)) into list: [1,2]
@@ -88,17 +105,20 @@ class Cheetah:
    def createTag(self, tag, connect=False):
       if connect: self.connect()
       cur = self.cur
+#     inserts tag into table
       cur.execute("INSERT INTO tags (tagName,tagID) VALUES (%r, NULL)" % tag)
+#     collects new id of tag for debugging purposes
       cur.execute("SELECT last_insert_id()")
       out = cur.fetchone()[0]
       logger.debug('tag: %s :: id: %s' % (tag, out))
       if connect:
+#!       apparently actions aren't saved without committing?
          self.conn.commit()
          self.close()
       return out
 
 #  finds tag from the given search term
-#  term can either be the tags name or its id
+#  term can either be the tags name or its id or a list of terms
    def findTag(self, term, connect=True):
       if connect: self.connect()
       cur = self.cur
@@ -117,73 +137,96 @@ class Cheetah:
       elif type(term) is list:
          tags = []
          for t in term:
-            tags.append(self.findTag(t))
+            tags.append(self.findTag(t),False)
+         out = tags
          logger.debug('term: %s :: found tag: %s' % (term, str(out)))
-         return out
       if connect: self.close()
       logger.debug('term: %s :: found tag: %s' % (term, str(out)))
+#     fixes out and returns it
       if out == tuple() or out is None: return None
-      return out[0]
+      if out is tuple: out = out[0]
+      return out
 
 #  parses string of tags to normalize them
 #  helps to prevent duplicates
    def parseTags(self, tags):
       x=tags
-#     changes dividing character from ; or , to `
+#     changes dividing character from ';' or ',' to '`'
+#     assumes method of division was consistent, so either ';'' or ',' but not both
       if ';' in tags:
          tags = tags.replace(';','`')
       else:
          tags = tags.replace(',','`')
-      tags = tags.split('`')
 #     splits tags
+      tags = tags.split('`')
+#     actual parsing of tags
       loop = []
-      for i,tag in enumerate(loop):
-         orig = tag
+      for tag in tags:
+#!       orig = tag
+#        attempts to fix escape characters
          tag = tag.encode('iso8859-1','ignore').decode('unicode_escape','ignore')
          if ':' in tag:
             tag = tag.split(':')
             tag = tag[-1]
+#        removes whitespace and makes all lowercase
          tag = tag.strip().lower()
+#        skips if tag is unreasonably long
          if len(tag) > 25:
             logger.debug('skipping tag: %s' % tag)
             continue
          loop.append(tag)
       logger.debug('input: %s :: output: %s' % (x, tags))
-      return tags
+      return loop
 
-#  uploads a set of article->tag pairs to article2tags
+#  uploads a set of article->tag links to article2tags
    def uploadTags(self, article_id, tag_ids, commit=False, connect=False):
       if connect: self.connect()
       cur = self.cur
 #     organizes data into tuple sets
       values = [(article_id,tag_id) for tag_id in tag_ids]
+#     changes tuple into string
       values = str(tuple(values))
+#     removes leading and trailing brackets '(' ')'
       values = values[1:-1]
       logger.debug('values: %s' % values)
-#     create request string
-      x=cur.execute("INSERT INTO article2tags (article,tag) VALUES %s" % values)
-      logger.debug('article_id: %i :: tag_ids: %s :: exec return: %s' % (article_id, tag_ids, x))
+#     create and execute request string
+      req = "INSERT INTO article2tags (article,tag) VALUES %s" % values
+      try:
+         x=cur.execute(req)
+      except:
+#        makes sure that request string gets logged. Actual error doesn't print it
+         logger.warning('error with request: %s' % req)
+         raise
+      logger.debug('article_id: %i :: tag_ids: %s :: exec return: %s :: mysql request: %s' % (article_id, tag_ids, x, req))
       if commit: self.conn.commit()
       if connect: self.close()
 
-#  gets list of tags paired
+#  given an article and a list of tags, creates a list of tag ids that go with the article id
+#  will create a new tag entry in table if one doesn't exist already. otherwise just reuses the old id number
    def putTags(self, article_id, tags, commit=False, connect=False):
       if type(tags) == str:
          tags = self.parseTags(tags)
+#     list of tag id numbers
       tag_IDs = []
       for tag in tags:
+#        checks if tag already exists
          tag_ID = self.findTag(tag, connect)
          if tag_ID:
             tag_IDs.append(tag_ID)
+#        otherwise creates a new tag
          else:
             tag_IDs.append(self.createTag(tag, True))
       tag_IDs = list(set(tag_IDs))
       logger.debug('article_id, tags, tag_IDs :: %i, %s, %s' % (article_id, tags, tag_IDs))
+#     uploads new tag associations
       self.uploadTags(article_id,tag_IDs, commit, connect)
 
+#  uploads article data to Article table. Data should be a list of dictionaries
+#  but can also use just one dictionary
    def putData(self, data, *values):
       self.connect()
       cur = self.cur_dict
+#     get base index number for debugging and associating tags
       cur.execute("SELECT MAX(id) as id from Articles")
       index = cur.fetchone()['id']
 #     cursor returns None if table is empty.
@@ -199,7 +242,7 @@ class Cheetah:
             #self.putTags(index + i, item['tags'], False)
             del(item['tags'])
 
-#        insert a default value to make data more comprehensive
+#        has mysql assign a date if none exists
          if 'date' not in item:
             data[i]['date'] = 'CURDATE()'
 
@@ -207,10 +250,17 @@ class Cheetah:
       logger.info('uploading data')
       for i in requests:
          keys = str(i[0])
+#        removes "'" from keys string
          keys = ''.join(keys.split("'"))
-         q = "INSERT INTO Articles %s VALUES %s;" % (keys, i[1])
-         logger.debug(q)
-         cur.execute(q)
+#        builds
+         req = "INSERT INTO Articles %s VALUES %s;" % (keys, i[1])
+         logger.debug('put data request: %s' % req)
+         try:
+            cur.execute(req)
+         except:
+#           makes sure that request string gets logged. Actual error doesn't print it
+            logger.warning('error with request: %s' % req)
+            raise
       self.conn.commit()
       for tag in tags:
          self.putTags(*tag, connect=False, commit=False)
@@ -218,7 +268,9 @@ class Cheetah:
       logger.info('upload complete')
       self.close()
 
+#  gets articles pinned by a certain user session
    def getPins(self,user,connect=True):
+#     makes username lowercase and stripped of white spaces
       user = user.lower().strip()
       if connect: self.connect()
       cur = self.cur
@@ -229,53 +281,66 @@ class Cheetah:
       if connect: self.close()
       return [x[0] for x in data]
 
+#  pins an article or set of article ids to a user session
    def addPins(self,user,ids,connect=True):
+#     makes username lowercase and stripped of white spaces
       user = user.lower().strip()
       if connect: self.connect()
       cur = self.cur
+#     makes list out of single int or string.
       if type(ids) is str or type(ids) is int:
          ids = [ids]
+#     checks for and removes duplicate pins
       current = set(self.getPins(user,False))
       ids = list(set(ids) - current)
+#     continues if list is not empty
       if ids != list():
          values = []
          for i in ids:
             values.append((user,i))
          q = "INSERT INTO sessions (name,article) VALUES %s" % str(values)[1:-1]
-         print(q)
-         logger.warn('query: %s' % q)
+         logger.debug('query: %s' % q)
          cur.execute(q)
          self.conn.commit()
       if connect: self.close()
       return self.getPins(user,connect)
+
+#  deletes a pinned id or set of id numbers from user session
    def delPins(self,user,ids,connect=True):
       user = user.lower().strip()
       if connect: self.connect()
       cur = self.cur
+#     makes tuple out of single int or string.
       if type(ids) is str or type(ids) is int:
          ids = (ids,)
+#     makes tuple out of list
       elif type(ids) is list:
          ids = tuple(ids)
+
+#     passes ids as tuple to mysql
       if len(ids) > 1:
          q = "DELETE FROM sessions WHERE name='%s' and article in %s" % (user,ids)
+#     passes single id to mysql
       elif len(ids) == 1:
          q = "DELETE FROM sessions WHERE name='%s' and article=%s" % (user,ids[0])
       else:
          q = None
 
-      print(q)
+      logger.debug('query: %s' % str(q))
       if q:
          cur.execute(q)
          self.conn.commit()
       if connect: self.close()
       return self.getPins(user,connect)
 
+#  closes a mysql connection
    def close(self):
       logger.info('connection closing')
       self.cur.close()
       self.cur_dict.close()
       self.conn.close()
 
+#  opens a mysql connection and creates cursors
    def connect(self):
       logger.info('connection opening')
       self.conn = pymysql.connect(host='localhost',user='extemp',passwd='zhH/5]qAYTNNXgE',db='forensics',charset='utf8',init_command="SET NAMES UTF8")
